4e8e5599b9c8ec859977c015b138c73a5b5672ab
Manfred Kaul <Manfred.Kaul@gmail.com>
2016-06-12 11:07:08 +0200

    outsourcing config from gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@
 // sudo npm install --global gulp-cli
 // npm install --save-dev gulp
 
+// Please edit config.json before starting
+const config = require('./config.json');
+
+// load npm modules and gulp plugins
 var fs = require('fs');
 var path = require('path');
 var gulp = require('gulp');
@@ -18,31 +22,25 @@ var gutil = require('gulp-util');
 var prompt = require('gulp-prompt');
 var runSequence = require('run-sequence');
 
-
-// we define some constants here so they can be reused
-var   USER = 'mkaul';
-const REPO =  'ccm-components';
-const SRC  = '../' + REPO + '/';
-const DEST = '../' + REPO + '-page/resources/';
-const SRC2 = 'external_components';
-const SERVER_URL = 'http://' + USER + '.github.io/' + REPO + '/resources/';
+// define constants for global use
+const USER = config.gulp.user;
+const REPO = config.gulp.repo;
+const SRC  = path.join( '..', REPO );
+const DEST = path.join( '..', REPO + config.gulp.doc_destination );
+const SRC2 = config.gulp.external_components;
+const SERVER_URL = param_replace( config.gulp.server_url, {'user': USER, 'repo': REPO } );
 
 gulp.task('default', [ 'js', 'css', 'json', 'doc' ]);
 
-gulp.task('doc_blank', function (folder, cb) {
-  // http://usejsdoc.org
-  folder = 'blank';
-  jsdoc3_config.opts.destination = '../'+REPO+'-page/api/' + folder;
-  gulp.src( [SRC+'/' + folder + '/*.js'], {read: false} )
-    .pipe(jsdoc(jsdoc3_config, cb));
-});
-
+// remotify JavaScript files by replacing local paths by remote paths
+// minify JavaScript files after remotifying
 gulp.task('js', function() {
   gulp.src(glob_pattern('js'))
     // the `changed` task needs to know the destination directory
     // upfront to be able to figure out which files changed
     .pipe(changed(DEST))
     // only files that has changed will pass through here
+    // replace local paths by remote paths
     .pipe(replace('./', SERVER_URL))
     .pipe(uglify())
     // .pipe(rename({
@@ -51,6 +49,7 @@ gulp.task('js', function() {
     .pipe(gulp.dest(DEST));
 });
 
+// minify CSS files
 gulp.task('css', function () {
   gulp.src(glob_pattern('css'))
     .pipe(changed(DEST))
@@ -65,6 +64,7 @@ gulp.task('css', function () {
     .pipe(gulp.dest(DEST));
 });
 
+// minify JSON files
 gulp.task('json', function () {
   return gulp.src(glob_pattern('json'))
     .pipe(changed(DEST))
@@ -79,63 +79,29 @@ gulp.task('json', function () {
     .pipe(gulp.dest(DEST));
 });
 
-/**************************************
-*                jsdoc3
+/* **************************************
+*              doc
+* generate jsdoc3 API documents from comments inside JavaScript files
+* using jsdoc3
 * generate extra gulp task per ccm-component
-*
-*
+* generate extra API documents per ccm-component
 * ************************************ */
 
-var exclude_folders = {
-  '.git': true,
-  '.idea': true,
-  'api': true,
-  'dist': true,
-  'fonts': true,
-  'images': true,
-  'javascripts': true,
-  'libs': true,
-  'stylesheets': true
-};
-
-jsdoc3_config = {
-  "tags": {
-    "allowUnknownTags": true
-  },
-  "source": {
-    "excludePattern": "(^|\\/|\\\\)_"
-  },
-  "opts": {
-    "destination": "see gulpfile.js"
-  },
-  "plugins": [
-    "plugins/markdown"
-  ],
-  "templates": {
-    "cleverLinks": false,
-    "monospaceLinks": false,
-    "default": {
-      "outputSourceFiles": true
-    },
-    "path": "ink-docstrap",
-    "theme": "cerulean",
-    "navType": "vertical",
-    "linenums": true,
-    "dateFormat": "MMMM Do YYYY, h:mm:ss a"
-  }
-};
+var exclude_folders = config.gulp.exclude_folders;
 
 var folders = getFolders(SRC);
 var task_names = [];
 
-// generate extra gulp task per ccm-component
+// iterate over all folders in REPO
 folders.map(function(folder) {
   if ( !folder || folder.length === 0 ) return;
   // console.log('>>> ' + path.join(SRC, folder, '/*.js'));
-  var sub_config = JSON.parse(JSON.stringify(jsdoc3_config)); // deep copy
+  var sub_config = JSON.parse(JSON.stringify(config)); // separate deep copy for each folder
   sub_config.opts.destination = '../'+REPO+'-page/api/' + folder;
   var task_name = 'doc_' + folder;
   task_names.push( task_name );
+
+  // generate extra gulp task per ccm-component, e.g. doc_blank
   return gulp.task( task_name, function (cb) {
     gulp.src([SRC + '/' + folder + '/*.js'], {read: false})
       .on('error', gutil.log)
@@ -143,8 +109,11 @@ folders.map(function(folder) {
   });
 });
 
-gulp.task('doc', function (cb) {
-  runSequence(task_names, cb);
+gulp.task('doc', function (done) {
+  runSequence(task_names, function () { // TODO to be replaced in gulp 4
+    console.log('=== doc task ready ===');
+    done();
+  });
 });
 
 function glob_pattern(suffix){
@@ -160,4 +129,10 @@ function getFolders(dir) {
     .filter(function(file) {
       return !exclude_folders[file] && fs.statSync(path.join(dir, file)).isDirectory();
     });
+}
+
+function param_replace( param_string, replace_object ) {
+  return param_string.replace(/\{(.*?)\}/g, function(match, param) {
+    return param in replace_object ? replace_object[param] : match;
+  });
 }
